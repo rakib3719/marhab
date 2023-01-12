1e3fc5679f2f39f6b921713ab3fa72d54d38d269
charles david <user@mx>
2023-01-12 02:32:10 +0100

    working tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App bg-slate-300 h-screen">
+      <div className="bg-green-300">Bad guy</div>
+    </div>
+  )
+}
+
+export default App
